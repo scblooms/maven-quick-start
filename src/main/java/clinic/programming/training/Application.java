a1c7b0eb165fa870a4b7d7af469931e655692f32
Saurav Chandra <chandra.saurav2000@gmail.com>
2018-01-04 15:43:27 +0530

    Adding Unit Testing

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 public class Application {
 
 public int countWords(String words){
-	String[] separateWords = StringUtils.split(words,"");
+	String[] separateWords = StringUtils.split(words," ");
 	return(separateWords == null)?0:separateWords.length;
 }
 
